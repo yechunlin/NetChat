58de732038646204e49f504f184805738cdef184
yechunlin <yechunlin@corp-ci.com>
2019-12-20 15:46:09 +0800

    web2

--- webSocketServer.php
+++ webSocketServer.php
@@ -4,13 +4,13 @@ class WebSocketServer
 	public  $ws;
 	public  $table;
 	public  $redis;
-	private $ssl = true;
+	static  $ssl = true;
 	private $connectConf = array('ip'=>'172.21.0.16', 'port'=> 8889);
 
 	public function __construct($conf = array()){
 		$conf = array_merge($this->connectConf, $conf);
 		$tcp = SWOOLE_SOCK_TCP;
-		if($this->ssl){
+		if($this::$ssl){
 			$tcp = SWOOLE_SOCK_TCP | SWOOLE_SSL;
 		}
 		$this->ws = new swoole_websocket_server($conf['ip'],$conf['port'],SWOOLE_PROCESS,$tcp);
@@ -25,7 +25,7 @@ class WebSocketServer
                                 'heartbeat_check_interval' => 300,
                                 'heartbeat_idle_time'      => 600
 		);
-		if($this->ssl){
+		if($this::$ssl){
 			$setConf['ssl_cert_file'] = '/usr/local/ssl/Nginx/1_www.yechunlin.com_bundle.crt';
 			$setConf['ssl_key_file'] = '/usr/local/ssl/Nginx/2_www.yechunlin.com.key';
 		}
@@ -60,11 +60,83 @@ class WebSocketServer
 		//启动触发
 		echo "服务启动\r\n";
 	}
-	public function onOpen($ws, $request){}
-	public function onMessage($ws, $frame){}
-	public function onTask($ws, $task_id, $from_id, $data){}
+	public function onOpen($ws, $request){
+		echo $request->fd." Joined successfully\r\n";
+	}
+	public function onMessage($ws, $frame){
+		parse_str($frame->data, $data);//解析字符串为数组
+		$data['id'] = $frame->fd;
+		$task_id = $ws->task($data);//投递task异步任务
+	}
+	public function onTask($ws, $task_id, $from_id, $data){
+		$this->workerFun($ws,$data);
+		$ws->finish('ok');
+	}
 	public function onFinish($ws, $task_id, $data){}
 	public function onClose($ws, $fd){}
+
+	public function workerFun($ws,$data){
+		global $this->redis;
+		$old_clients = $ws->table;//连接池
+		//新人
+		if($data['flag'] == 'new'){
+			$clients = array();
+			foreach($ws->table as $key => $val){
+				$clients[$key] = $val;
+			}
+			$old_clients = $clients;
+			$clients[$data['id']] = array(
+				'userid'   => $data['id'],
+				'nickname' => $data['nickname'],
+				'img'      => $data['img']
+			);
+			$ws->table->set($data['id'],$clients[$data['id']]);
+			$data['clients'] = $clients;
+			$ws->push($data['id'], json_encode($data));//新人进来单独发送一份消息
+			unset($data['clients']);
+		}
+
+		//@召唤
+		if(isset($data['msg'])){
+			preg_match_all('/data-to="(.*?)"/', $data['msg'], $arr_to);
+			if(!empty($arr_to[1])){
+				$data['at'] = $arr_to[1];
+				$data['msg'] .= "&nbsp";
+			}
+		}
+	
+		$data['img'] = $ws->table->get($data['id'],'img');
+		$data['nickname'] = $ws->table->get($data['id'],'nickname');
+
+		//连续五分钟内无交互，显示一次时间
+		if(!$this->redis->get('web_socket_time')){
+			$this->redis->set('web_socket_time', 1, 300);
+			$data['date'] = date('Y-m-d H:i:s');
+		}
+		$this->redis->set('web_socket_time', 1, 300);
+
+		if(isset($data['private'])){
+			//私聊
+			$sl_arr[$data['id']] = $data['id'];
+			$sl_arr[$data['for_id']] = $data['for_id'];
+			$userid     = $ws->table->get($data['id'],'userid');
+			$for_userid = $ws->table->get($data['for_id'],'userid');
+			$this->sendMsg($ws,$sl_arr,$data);
+			//$this->savemsg($redis, $userid, $for_userid, $data);
+		}else{
+			//群聊
+			if($old_clients){
+				$this->sendMsg($ws,$old_clients,$data);
+			}
+		}
+	}
+
+	//发送数据
+	public function sendMsg($ws,$clients,$data){
+		foreach($clients as $fd => $name){
+        	$ws->push($fd, json_encode($data));
+		}	
+	}
 
 }
 new WebSocketServer();
